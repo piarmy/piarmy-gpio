502e288623838680a525b6aa27dbc4462269421a
Matt J. Wiater <mwiater@users.noreply.github.com>
2017-05-26 08:15:10 -0700

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 
-docker rm --force $(docker ps | awk '/piarmy-gpio/{ print $1 }') && \
+docker rm --force $(docker ps | awk '/piarmy-gpio/{ print $1 }') || echo "No running image..." && \
   docker build -t mattwiater/piarmy-gpio .  && \
   docker run -d --rm --privileged --network host --cap-add SYS_RAWIO --device /dev/mem -p:1880:1880 --name=piarmy-gpio mattwiater/piarmy-gpio && \
   image=$(docker ps | awk '/piarmy-gpio/{ print $1 }') && \
