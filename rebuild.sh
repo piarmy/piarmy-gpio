bf85493e8d2cc72f2b6ba4a8860d84f747136399
Matt J. Wiater <matt@brightpixel.com>
2017-06-17 10:44:56 -0700

    privileged

--- rebuild.sh
+++ rebuild.sh
@@ -2,10 +2,12 @@
 
 docker rm --force $(docker ps | awk '/piarmy-gpio/{ print $1 }') || echo "No running image..." && \
   docker build -t mattwiater/piarmy-gpio .  && \
-  docker run -d --rm --privileged --network host --cap-add SYS_RAWIO --device /dev/mem -p:1880:1880 --name=piarmy-gpio mattwiater/piarmy-gpio && \
+  docker run -d --rm --privileged --network host -p:1880:1880 --name=piarmy-gpio mattwiater/piarmy-gpio && \
   image=$(docker ps | awk '/piarmy-gpio/{ print $1 }') && \
   echo $image
 
 docker commit $image mattwiater/piarmy-gpio
 docker push mattwiater/piarmy-gpio
 docker rm --force $image
+
+docker run -d --restart=unless-stopped --privileged --network host -p:1880:1880 --name=piarmy-gpio mattwiater/piarmy-gpio
